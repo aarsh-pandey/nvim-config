f4f8af2109045edff09894434a5267ffe4718b00
Aarsh Pandey <aarsh@Aarshs-MacBook-Air-2017.local>
2025-03-29 10:50:47 +0530

    highlight when yank is pressed

--- init.lua
+++ init.lua
@@ -43,6 +43,22 @@ vim.g.mapleader = " " -- Spacebar as the leader key
 -- Set the local leader key
 vim.g.maplocalleader = "\\"
 
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 vim.api.nvim_create_autocmd("TermOpen", {
   pattern = "*",
   callback = function()
